d8f59497c4a20d629686863db69035c20a21dd89
CJ Rosario <cjricegod@gmail.com>
2024-03-19 15:20:21 +0800

    /users/ only shows email

--- backend-login-system/routes/users.js
+++ backend-login-system/routes/users.js
@@ -4,7 +4,7 @@ const User = require('../models/user');
 
 router.get('/', async (req, res) => {
     try {
-        const users = await User.find();
+        const users = await User.find().select('email');
         res.status(200).json(users);
     } catch (err) {
         res.status(500).json({ message: err.message });
