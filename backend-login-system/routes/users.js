45b18a9b987d67299744627009a9fcd30f53f37a
CJ Rosario <cjricegod@gmail.com>
2024-03-24 17:03:45 +0800

    used generic error returns for functions
    to test in server.js

--- backend-login-system/routes/users.js
+++ backend-login-system/routes/users.js
@@ -6,14 +6,12 @@ async function doesUserExist(email) {
         const user = await User.findOne({ email });
         return user != null;
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        throw new Error(err.message);
     }
 }
 
 async function isCommonPassword(password) {
-    if (await commonPasswords(password)) {
-        return res.status(400).json({ message: 'Password is too common' });
-    }
+    return await commonPasswords(password);
 }
 
 export {doesUserExist, isCommonPassword};
