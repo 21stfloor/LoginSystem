1031a8b6dcddd21d6c2a6870d16b347645e6ae41
John Erick Arzaga <arzagaerick11@gmail.com>
2024-04-01 22:50:35 +0800

    Updated DatePicker with year dropdown

--- frontend-login-system/src/components/calendar/DatePicker.tsx
+++ frontend-login-system/src/components/calendar/DatePicker.tsx
@@ -46,32 +46,31 @@ export function DatePicker() {
               <Popover>
                 <PopoverTrigger asChild>
                   <FormControl>
-                    <Button
-                      variant={"outline"}
-                      className={cn(
-                        "w-full pl-3 text-left font-normal",
-                        !field.value && "text-muted-foreground"
-                      )}
-                    >
-                      {field.value ? (
-                        format(field.value, "PPP")
-                      ) : (
-                        <span>Pick a date</span>
-                      )}
-                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                    </Button>
+                  <Button
+                    variant={"outline"}
+                    className={cn(
+                      "w-full pl-3 justify-start text-left font-normal",
+                      !field.value && "text-muted-foreground"
+                    )}
+                  >
+                    <CalendarIcon className="mr-2 h-4 w-4" />
+                    {field.value ? (
+                      format(field.value, "PPP")
+                    ) : (
+                      <span>Pick a date</span>
+                    )}
+                  </Button>
                   </FormControl>
                 </PopoverTrigger>
                 <PopoverContent className="w-auto p-0" align="start">
-                  <Calendar
-                    mode="single"
-                    selected={field.value}
-                    onSelect={field.onChange}
-                    disabled={(date) =>
-                      date > new Date() || date < new Date("1900-01-01")
-                    }
-                    initialFocus
-                  />
+                <Calendar
+                  mode="single"
+                  captionLayout="dropdown-buttons"
+                  selected={field.value}
+                  onSelect={field.onChange}
+                  fromYear={1920}
+                  toYear={new Date().getFullYear()}
+                />
                 </PopoverContent>
               </Popover>
               <FormMessage />
