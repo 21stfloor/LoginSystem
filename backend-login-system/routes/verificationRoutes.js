d287584a8f11f18645668eaf43c232e048d7dd2a
21stfloor <21stfloordev@gmail.com>
2024-04-08 19:39:06 +0800

    implemented feature #21

--- backend-login-system/routes/verificationRoutes.js
+++ backend-login-system/routes/verificationRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { generateVerificationToken, doesTokenExist } from '../controllers/authController.js';
+import VerificationTokenModel from '../models/verificationTokenModel.js';
 
 const ROUTER = express.Router();
 
@@ -11,6 +12,25 @@ ROUTER.post('/create-token', async (req, res) => {
     } else {
         const token = generateVerificationToken();
         return res.status(200).json({ message: "Success", token });
+    }
+});
+
+ROUTER.get('/verify/:id', async (req, res) => {
+    const VERIFICATION_ID = req.params.id;
+    if (!VERIFICATION_ID) {    
+        return res.status(400).send('Bad Request');
+    }
+
+    try {
+        const DELETED_TOKEN = await VerificationTokenModel.findOneAndDelete({ id: VERIFICATION_ID });
+
+        if (!DELETED_TOKEN) {
+            return res.status(404).send('Verification token not found');
+        }
+
+        return res.redirect('/login');
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
 });
 
