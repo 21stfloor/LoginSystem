d8bc774e829964915726c12f7a0bd8bedcf786d6
CJ Rosario <cjricegod@gmail.com>
2024-04-09 18:52:09 +0800

    added mongodb transaction

--- backend-login-system/routes/verificationRoutes.js
+++ backend-login-system/routes/verificationRoutes.js
@@ -1,16 +1,38 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import { generateVerificationToken, doesTokenExist } from '../controllers/authController.js';
+import VerificationToken from '../models/verificationTokenModel.js';
 
 const ROUTER = express.Router();
 
 ROUTER.post('/create-token', async (req, res) => {
     const EMAIL = req.body.email;
-    const TOKEN_EXISTS = await doesTokenExist(EMAIL);
-    if (TOKEN_EXISTS) {
-        return res.status(400).json({ error: 'Token already exists' });
-    } else {
-        const token = generateVerificationToken();
-        return res.status(200).json({ message: "Success", token });
+
+    const SESSION = await mongoose.startSession();
+    SESSION.startTransaction();
+
+    try {
+        const TOKEN_EXISTS = await doesTokenExist(EMAIL); // Pass session to the function
+        if (TOKEN_EXISTS) {
+            await SESSION.abortTransaction();
+            await SESSION.endSession();
+            return res.status(400).json({ error: 'Token already exists' });
+        } else {
+            const TOKEN = generateVerificationToken();
+            const NEW_TOKEN = new VerificationToken({
+                _id: TOKEN,
+                email: EMAIL
+            });
+            await NEW_TOKEN.save({ session: SESSION });
+            await SESSION.commitTransaction();
+            await SESSION.endSession();
+            return res.status(200).json({ message: "Success", token: TOKEN });
+        }
+    } catch (error) {
+        await SESSION.abortTransaction();
+        await SESSION.endSession();
+        console.error(error);
+        return res.status(500).json({ error: 'Internal server error' });
     }
 });
 
