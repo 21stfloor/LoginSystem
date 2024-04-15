526c4b3c69bfc498634055325f869825479cc343
21stfloor <21stfloordev@gmail.com>
2024-04-15 16:37:58 +0800

    removed create-token endpoint

--- backend-login-system/routes/verificationRoutes.js
+++ backend-login-system/routes/verificationRoutes.js
@@ -1,19 +1,7 @@
 import express from 'express';
-import { generateVerificationToken, doesTokenExist } from '../controllers/authController.js';
 import VerificationTokenModel from '../models/verificationTokenModel.js';
 
 const ROUTER = express.Router();
-
-ROUTER.post('/create-token', async (req, res) => {
-    const EMAIL = req.body.email;
-    const TOKEN_EXISTS = await doesTokenExist(EMAIL);
-    if (TOKEN_EXISTS) {
-        return res.status(400).json({ error: 'Token already exists' });
-    } else {
-        const token = generateVerificationToken();
-        return res.status(200).json({ message: "Success", token });
-    }
-});
 
 ROUTER.get('/verify/:id', async (req, res) => {
     const VERIFICATION_ID = req.params.id;
