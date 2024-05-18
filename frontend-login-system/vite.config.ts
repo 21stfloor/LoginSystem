7e58eb1c16550a06b546972cdbb2388304aacb36
John Erick Arzaga <arzagaerick11@gmail.com>
2024-05-18 22:40:58 +0800

    modified vite.config.ts

--- frontend-login-system/vite.config.ts
+++ frontend-login-system/vite.config.ts
@@ -1,32 +1,31 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import dotenv from 'dotenv'
-
-dotenv.config()
-
-const env = process.env.VITE_ENV
-
-const baseURL = env === 'development' ? process.env.VITE_DEV_BASE_URL : process.env.VITE_PROD_BASE_URL
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: baseURL,
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const baseURL = env.VITE_DEV_BASE_URL; 
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
-  build: {
-    outDir: 'dist',
-  },
+    define: {
+      __BASE_URL__: JSON.stringify(baseURL), 
+    },
+    server: {
+      proxy: {
+        '/api': {
+          target: baseURL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
